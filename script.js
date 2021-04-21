1ca5108b7c13394ffcb123604e48ac4ff6464a85
ThomasFoydel <thomasjfoydel@gmail.com>
2021-04-21 17:21:31 -0500

    bot image, styles and layout

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
-const applyBtn = document.getElementById('apply');
 const outputEl = document.getElementById('output');
 const outputBin = document.getElementById('output-bin');
 const outputHex = document.getElementById('output-hex');
 const displayOne = document.getElementById('display-one');
 const displayTwo = document.getElementById('display-two');
-let inputOne = document.getElementById('input-one');
-let inputTwo = document.getElementById('input-two');
+const selector = document.getElementById('selector');
+const inputOne = document.getElementById('input-one');
+const inputTwo = document.getElementById('input-two');
+
 const bitOps = {
   and: (a, b) => a & b,
   or: (a, b) => a | b,
@@ -16,7 +17,7 @@ const bitOps = {
   rightShiftZero: (a, b) => a >>> b,
 };
 
-applyBtn.addEventListener('click', () => {
+const calculate = () => {
   const valueOne = inputOne.value;
   const valueTwo = inputTwo.value;
   const op = document.getElementById('selector').value;
@@ -26,19 +27,22 @@ applyBtn.addEventListener('click', () => {
     outputBin.textContent = (output >>> 0).toString(2);
     outputHex.textContent = Number(output).toString(16);
   }
-});
+};
+
+selector.addEventListener('change', calculate);
 
 const handleInput = ({ target: { value, id } }) => {
   const bin = (value >>> 0).toString(2);
   const hex = Number(value).toString(16);
-
+  console.dir(displayOne);
   if (id === 'input-one') {
-    displayOne.childNodes[0].textContent = bin;
-    displayOne.childNodes[1].textContent = hex;
+    displayOne.children[0].textContent = bin;
+    displayOne.children[1].textContent = hex;
   } else if (id === 'input-two') {
-    displayTwo.childNodes[0].textContent = bin;
-    displayTwo.childNodes[1].textContent = hex;
+    displayTwo.children[0].textContent = bin;
+    displayTwo.children[1].textContent = hex;
   }
+  calculate();
 };
 
 [inputOne, inputTwo].forEach((i) =>
